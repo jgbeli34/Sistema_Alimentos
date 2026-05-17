d829626bcf1ec2cf1d61cfc2519bedfdc9271fbc
jOsE0134 <josed.ramirezb@konradlorenz.edu.co>
2026-05-16 23:03:29 -0500

    Se agrega archivo de modulos de software stand-alone,web y movil codificados y probados

--- backend/guardar.php
+++ backend/guardar.php
@@ -1,10 +1,17 @@
 <?php
+
 include "conexion.php";
 
+// Capturar datos del formulario
 $nombre = $_POST["nombre"];
 $precio = $_POST["precio"];
 
-$conexion->query("INSERT INTO productos (nombre, precio) VALUES ('$nombre', '$precio')");
+// Insertar producto
+$sql = "INSERT INTO productos(nombre, precio)
+VALUES('$nombre', '$precio')";
 
-header("Location: productos.php");
+$conexion->query($sql);
+
+header("Location: ../frontend/productos.php");
+
 ?>
